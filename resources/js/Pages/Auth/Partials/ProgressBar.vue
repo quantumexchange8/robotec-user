956bfc38155ad596f098348b41af0ad2886e6d1e
choo <choohuanlong@gmail.com>
2024-06-05 13:40:52 +0800

    add progress bar in register

---
+++ resources/js/Pages/Auth/Partials/ProgressBar.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { WhiteCheckIcon } from '@/Components/Icons/solid';
+import { ref } from 'vue';
+
+const progress = ref('0');
+</script>
+
+<template>
+    <div class="w-full py-6">
+        <div 
+            v-if="progress === '0'"
+            class="mx-auto w-48 relative flex justify-center items-center"
+        >
+            <!-- Left shape -->
+            <div class="absolute left-0 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                <div class="flex flex-col items-center">
+                    <div class="w-6 h-6 text-lg text-white flex items-center bg-gray-700 rounded-full border-[3px] border-success-500 justify-center">
+                        <div class="w-2 h-2 rounded-full text-lg text-white flex items-center bg-success-500"></div>
+                    </div>
+                    <div class="text-center text-white text-xs font-medium">Profile Info</div>
+                </div>
+            </div>
+
+            <!-- Progress bar -->
+            <div class="absolute left-0 bottom-1.5 w-full h-1 bg-gray-700 rounded-[20px]"></div>
+
+            <!-- Right shape -->
+            <div class="absolute right-0 top-1/2 transform translate-x-1/2 -translate-y-1/2 z-10">
+                <div class="flex flex-col items-center">
+                    <div class="w-6 h-6 text-lg text-white flex items-center rounded-full bg-gray-700 justify-center"></div>
+                    <div class="text-center text-white text-xs font-medium">Create Password</div>
+                </div>
+            </div>
+        </div>
+
+        <div 
+            v-if="progress === '1'"
+            class="mx-auto w-48 relative flex justify-center items-center"
+        >
+            <!-- Left shape -->
+            <div class="absolute left-0 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                <div class="flex flex-col items-center">
+                    <div class="w-6 h-6 text-lg flex items-center rounded-full bg-success-500 justify-center">
+                        <WhiteCheckIcon />
+                    </div>
+                    <div class="text-center text-white text-xs font-medium">Profile Info</div>
+                </div>
+            </div>
+
+            <!-- Progress bar -->
+            <div class="absolute left-0 bottom-1.5 w-full h-1 rounded-[20px]">
+                <!-- Left half -->
+                <div class="absolute left-0 top-0 w-1/2 h-full bg-success-500"></div>
+                <!-- Right half -->
+                <div class="absolute right-0 top-0 w-1/2 h-full bg-gray-700"></div>
+            </div>
+
+            <!-- Center dot -->
+            <div class="absolute z-10 left-[50%] -bottom-1 transform -translate-x-1/2 -translate-y-1/2">
+                <div class="relative">
+                    <div class="w-3 h-3 absolute bottom-1.5 -right-1.5 flex items-center bg-success-900 rounded-full justify-center opacity-50"></div>
+                    <div class="w-2 h-2 absolute bottom-2 -right-1 rounded-full flex items-center bg-success-500 border-2 border-gray-700"></div>
+                </div>
+            </div>
+
+            <!-- Right shape -->
+            <div class="absolute right-0 top-1/2 transform translate-x-1/2 -translate-y-1/2 z-10">
+                <div class="flex flex-col items-center">
+                    <div class="w-6 h-6 text-lg flex items-center rounded-full bg-gray-700 justify-center"></div>
+                    <div class="text-center text-white text-xs font-medium">Create Password</div>
+                </div>
+            </div>
+        </div>
+
+        <div 
+            v-if="progress === '2'"
+            class="mx-auto w-48 relative flex justify-center items-center"
+        >
+            <!-- Left shape -->
+            <div class="absolute left-0 top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+                <div class="flex flex-col items-center">
+                    <div class="w-6 h-6 text-lg flex items-center rounded-full bg-success-500 justify-center">
+                        <WhiteCheckIcon />
+                    </div>
+                    <div class="text-center text-white text-xs font-medium">Profile Info</div>
+                </div>
+            </div>
+
+            <!-- Progress bar -->
+            <div class="absolute left-0 bottom-1.5 w-full h-1 bg-success-500 rounded-[20px]"></div>
+
+            <!-- Right shape -->
+            <div class="absolute right-0 top-1/2 transform translate-x-1/2 -translate-y-1/2 z-10">
+                <div class="flex flex-col items-center">
+                    <div class="w-6 h-6 text-lg flex items-center rounded-full bg-success-500 justify-center">
+                        <WhiteCheckIcon />
+                    </div>
+                    <div class="text-center text-white text-xs font-medium">Create Password</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -2,10 +2,11 @@
 import GuestLayout from '@/Layouts/GuestLayout.vue';
 import InputError from '@/Components/InputError.vue';
 import Label from '@/Components/Label.vue';
-import PrimaryButton from '@/Components/PrimaryButton.vue';
+import Button from '@/Components/Button.vue';
 import Input from '@/Components/Input.vue';
 import { Head, Link, useForm } from '@inertiajs/vue3';
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
+import ProgressBar from '@/Pages/Auth/Partials/ProgressBar.vue'
 
 const form = useForm({
     name: '',
@@ -26,123 +27,110 @@ const submit = () => {
 
         <Head title="Register" />
 
-        <div class="flex flex-col items-center justify-center mb-10">
+        <div class="px-4 pb-10 flex flex-col items-center gap-3 self-stretch">
             <Link href="/">
-            <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <ApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
             </Link>
-            <div class="text-center mt-3">
-                <div class="mb-2 text-white text-xl font-semibold">Create Your Account</div>
-                <div class="text-gray-300">Welcome! Fill in the details below to join our community. Let's get started!
+            <div class="flex flex-col items-center gap-2 self-stretch">
+                <div class="text-white text-center text-lg font-semibold">
+                    Create Your Account
                 </div>
-            </div>
-        </div>
-        <!-- <div class="w-full py-6 relative flex justify-center items-center">
-            <div class="absolute left-[30%] top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 mb-2">
-                <div class="flex flex-col items-center">
-                    <div
-                        class="w-6 h-6 text-lg text-white flex items-center bg-gray-800 rounded-full border-[3px] border-success-500 justify-center">
-                        <div class="w-2 h-2 rounded-full text-lg text-white flex items-center bg-success-500"></div>
-                    </div>
-                    <div class="text-center text-white text-xs font-medium">Profile Info</div>
-                </div>
-            </div>
-
-            <div class="absolute left-[30%] top-3 w-40 h-1 bg-gray-800 rounded-[20px]"></div>
-
-            <div class="absolute right-[30%] top-1/2 transform translate-x-1/2 -translate-y-1/2 z-10 mb-2">
-                <div class="flex flex-col items-center">
-                    <div
-                        class="w-6 h-6 text-lg text-white flex items-center rounded-full bg-gray-800 justify-center">
-                    </div>
-                    <div class="text-center text-white text-xs font-medium">Create Password</div>
-                </div>
-            </div>
-        </div> -->
-
-        <div class="w-full py-6 relative flex justify-center items-center">
-            <!-- Left shape -->
-            <div class="absolute left-[30%] top-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 mb-2">
-                <div class="flex flex-col items-center">
-                    <div
-                        class="w-6 h-6 text-lg flex items-center bg-gray-800 rounded-full border-[3px] border-success-500 justify-center">
-                        <div class="w-2 h-2 rounded-full text-lg flex items-center bg-success-500"></div>
-                    </div>
-                    <div class="text-center text-white text-xs font-medium">Profile Info</div>
-                </div>
-            </div>
-
-            <!-- Progress bar -->
-            <div class="absolute left-[30%] top-3 w-40 h-1 rounded-[20px] overflow-hidden">
-                <!-- Left half -->
-                <div class="absolute left-0 top-0 w-1/2 h-full bg-success-500"></div>
-                <!-- Right half -->
-                <div class="absolute right-0 top-0 w-1/2 h-full bg-gray-800"></div>
-                <!-- Center dot -->
-                <div class="absolute z-100 left-[50%] top-[50%] transform -translate-x-1/2 -translate-y-1/2">
-                    <div class="w-3 h-3 text-lg flex items-center bg-success-900 rounded-full justify-center">
-                        <div class="w-1 h-1 rounded-full text-lg flex items-center bg-success-500"></div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Right shape -->
-            <div class="absolute right-[30%] top-1/2 transform translate-x-1/2 -translate-y-1/2 z-10 mb-2">
-                <div class="flex flex-col items-center">
-                    <div
-                        class="w-6 h-6 text-lg flex items-center rounded-full bg-gray-800 justify-center">
-                    </div>
-                    <div class="text-center text-white text-xs font-medium">Create Password</div>
+                <div class="self-stretch text-gray-300 text-center text-sm">
+                    Welcome! Fill in the details below to join our community. Let's get started!
                 </div>
             </div>
         </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <Label for="name" value="Name" class="text-gray-300" :invalid="form.errors.name" />
+        <ProgressBar />
 
-                <Input id="name" type="text" class="mt-1 block w-full" v-model="form.name" required autofocus
-                    autocomplete="name" />
+        <form 
+            @submit.prevent="submit"
+            class="px-4 pt-3 flex flex-col items-center gap-10 self-stretch"
+        >
+            <div class="flex flex-col items-center gap-3 self-stretch">
+                <div class="flex flex-col items-start gap-1.5 self-stretch">
+                    <Label for="fullName" value="Full Name"/>
+                    <Input
+                        v-model="fullName"
+                        id="fullName"
+                        type="text"
+                        class="block w-full"
+                        placeholder="Name as per NRIC"
+                    />
+                    <InputError :message="form.errors.fullName" />
+                </div>
 
-                <InputError class="mt-2" :message="form.errors.name" />
+                <div class="flex flex-col items-start gap-1.5 self-stretch">
+                    <Label for="email" value="Email"/>
+                    <Input
+                        v-model="email"
+                        id="email"
+                        type="text"
+                        class="block w-full"
+                        placeholder="Enter Email"
+                    />
+                    <InputError :message="form.errors.email" />
+                </div>
+
+                <div class="flex flex-col items-start gap-1.5 self-stretch">
+                    <Label for="phoneNumber" value="Phone Number"/>
+                    <Input
+                        v-model="phoneNumber"
+                        id="phoneNumber"
+                        type="text"
+                        class="block w-full"
+                        placeholder="Phone Number"
+                    />
+                    <InputError :message="form.errors.phoneNumber" />
+                </div>
+
+                <div class="flex flex-col items-start gap-1.5 self-stretch">
+                    <Label for="password" value="Create Password"/>
+                    <Input
+                        v-model="password"
+                        id="password"
+                        type="password"
+                        class="block w-full"
+                        placeholder="New password"
+                    />
+                    <div class="self-stretch text-gray-300 text-xs">
+                        Must be at least 8 characters containing one uppercase letter, one lowercase letter, one number, and one special character.
+                    </div>
+                    <InputError :message="form.errors.password" />
+                </div>
+
+                <div class="flex flex-col items-start gap-1.5 self-stretch">
+                    <Label for="confirmPassword" value="Confirm Password"/>
+                    <Input
+                        v-model="password_confirmation"
+                        id="confirmPassword"
+                        type="text"
+                        class="block w-full"
+                        placeholder="Must match with new password"
+                    />
+                    <InputError :message="form.errors.password_confirmation" />
+                </div>
             </div>
 
-            <div class="mt-4">
-                <Label for="email" value="Email" class="text-gray-300" :invalid="form.errors.email" />
+            <div class="flex flex-col justify-center items-center self-stretch">
+                <Button
+                    size="lg"
+                    class="w-full font-semibold"
+                >
+                    Next
+                </Button>
 
-                <Input id="email" type="email" class="mt-1 block w-full" v-model="form.email" required
-                    autocomplete="username" />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <Label for="password" value="Password" class="text-gray-300" :invalid="form.errors.password" />
-
-                <Input id="password" type="password" class="mt-1 block w-full" v-model="form.password" required
-                    autocomplete="new-password" />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="mt-4">
-                <Label for="password_confirmation" value="Confirm Password" class="text-gray-300"
-                    :invalid="form.errors.password_confirmation" />
-
-                <Input id="password_confirmation" type="password" class="mt-1 block w-full"
-                    v-model="form.password_confirmation" required autocomplete="new-password" />
-
-                <InputError class="mt-2" :message="form.errors.password_confirmation" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link :href="route('login')"
-                    class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                Already registered?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Register
-                </PrimaryButton>
+                <div class="flex py-3 justify-center items-center gap-3 self-stretch">
+                    <div class="text-gray-300 text-center text-sm">
+                        Already have an account?
+                    </div>
+                    <Link
+                        :href="route('login')"
+                        class="text-white text-center text-sm font-semibold"
+                    >
+                        Log in here
+                    </Link>
+                </div>
             </div>
         </form>
     </GuestLayout>
