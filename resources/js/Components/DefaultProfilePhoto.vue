93537780d24aecaa2679d8bb8704b96014dced2f
Wayne Lai <waynelai06@gmail.com>
2024-06-26 18:30:17 +0800

    fix: profile icon padding

--- resources/js/Components/DefaultProfilePhoto.vue
+++ resources/js/Components/DefaultProfilePhoto.vue
@@ -3,7 +3,7 @@ import { ProfileIcon } from '@/Components/Icons/solid';
 </script>
 
 <template>
-    <div class="w-full h-full flex justify-center items-center bg-[#1C2433]">
+    <div class="w-full h-full p-1 flex justify-center items-center bg-[#1C2433]">
         <ProfileIcon />
     </div>
 </template>
