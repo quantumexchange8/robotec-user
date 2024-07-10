b3d52a8e9e721bb8133d19d3ba60e70e783abd54
Wayne Lai <waynelai06@gmail.com>
2024-07-10 17:06:12 +0800

    fix: auto zoom ios

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import { i18nVue } from 'laravel-vue-i18n'
+import iosZoomFix from '../js/Composables/ios-zoom-fix.js';
 
 const appName =
     window.document.getElementsByTagName('title')[0]?.innerText || 'K UI'
@@ -18,17 +19,21 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue')
         ),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .use(i18nVue, {
                 resolve: async lang => {
                     const langs = import.meta.glob('../../lang/*.json');
-                    if (typeof langs[`../../lang/${lang}.json`] == "undefined") return; //Temporary workaround
+                    if (typeof langs[`../../lang/${lang}.json`] == "undefined") return; // Temporary workaround
                     return await langs[`../../lang/${lang}.json`]();
                 }
-            })
-            .mount(el)
+            });
+
+        app.mount(el);
+
+        // Run the iOS zoom fix
+        iosZoomFix();
     },
     progress: {
         color: '#074B9F'
